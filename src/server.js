7ba4430fcd781a3b25601d40bb526fc0e460a50b
vinhnguyen <vinhnpgcc19003@fpt.edu.vn>
2023-08-03 11:29:00 +0700

    conect data base

--- src/server.js
+++ src/server.js
@@ -2,12 +2,17 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
+import bodyParser from "body-parser";
+
+
 
 const app = express();
 const PORT = process.env.PORT || 9090;
 
 configViewEngine(app);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 initWebRoutes(app);
 
